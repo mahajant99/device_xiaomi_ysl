0879ec35c5148879c2fd20afeb57bd9456d3ea06
Bruno Martins <bgcngm@gmail.com>
2019-01-21 09:40:51 +0000

    ysl: Migrate to livedisplay 2.0
    Change-Id: I513c284c534ce3abc27bf374513280171454f4d1

--- device.mk
+++ device.mk
@@ -230,9 +230,9 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.light@2.0-service.ysl
 	
-# LiveDisplay native
+# LiveDisplay
 PRODUCT_PACKAGES += \
-    vendor.lineage.livedisplay@1.0-service-sdm
+    vendor.lineage.livedisplay@2.0-service-sdm
 
 # Media
 PRODUCT_COPY_FILES += \
