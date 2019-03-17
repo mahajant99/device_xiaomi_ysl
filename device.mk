e79567921937656bf3cae4f63e3bc0a1aedd6492
TheStrix <parthbhatia98@gmail.com>
2019-03-17 17:20:35 +0530

    ysl: Add pa specific overlay
    * Enable double tap to wake

--- device.mk
+++ device.mk
@@ -25,6 +25,7 @@ $(call inherit-product, frameworks/native/build/phone-xhdpi-4096-dalvik-heap.mk)
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
+DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay-pa
 
 PRODUCT_ENFORCE_RRO_TARGETS := \
     framework-res
