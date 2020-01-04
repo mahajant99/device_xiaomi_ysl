3e6952fb7ffe64005b8615bad6cd4b7c89b94a3c
Michael Bestas <mkbestas@lineageos.org>
2020-01-04 15:40:59 +0000

    msm8953-common: Disable DPM features
    •Stops it from crashing, most devices have it disabled anyway.

--- prop.mk
+++ prop.mk
@@ -61,7 +61,7 @@ vidc.enc.dcvs.extra-buff-count=2
 # Cne/Dpm
 PRODUCT_PROPERTY_OVERRIDES += \
 persist.vendor.cne.feature=1 \
-persist.dpm.feature=1
+persist.vendor.dpm.feature=0
 
 # Coresight
 PRODUCT_PROPERTY_OVERRIDES += \
