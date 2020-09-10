8c14a2dee4861360d961dfae0b65dd3dcd219609
iamsaalim <saalimquadri1@gmail.com>
2020-09-10 11:21:04 +0800

    ysl: Set gnss manifest from device side
    * Seems copying the xml from vendor side throws up error
    
    Signed-off-by: iamsaalim <saalimquadri1@gmail.com>
    Signed-off-by: starlight5234 <starlight5234@protonmail.ch>
    Signed-off-by: Tushar Mahajan <mahajant99@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -127,6 +127,8 @@ TARGET_NO_RPC := true
 # HIDL
 DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/manifest.xml
 DEVICE_MATRIX_FILE   := $(DEVICE_PATH)/compatibility_matrix.xml
+DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/vintf/android.hardware.gnss@2.0-service-qti.xml
+DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/vintf/vendor.qti.gnss@3.0-service.xml
 
 # HWUI
 HWUI_COMPILE_FOR_PERF := true
