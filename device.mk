a0b92359b684b3f2303d3d02a49f3666b3a51182
Michael Bestas <mkbestas@lineageos.org>
2020-12-27 15:56:42 +0200

    ysl: Remove libantradio from PRODUCT_PACKAGES
    * It's proprietary and already included in vendor makefiles.
    
    Change-Id: Ic19703a39e3e691a04b226b2cb2a27b02703724d
    Signed-off-by: Aayush Mahajan <aayushmahajan950@gmail.com>

--- device.mk
+++ device.mk
@@ -80,8 +80,7 @@ PRODUCT_COPY_FILES += \
 # ANT
 PRODUCT_PACKAGES += \
     AntHalService \
-    com.dsi.ant.antradio_library \
-    libantradio
+    com.dsi.ant.antradio_library
 
 # Audio
 PRODUCT_PACKAGES += \
